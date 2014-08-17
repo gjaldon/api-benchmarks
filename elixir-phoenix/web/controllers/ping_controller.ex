6277448ad8ed6225c9a28203d77db09bf8198a9f
Gabriel Jaldon <gjaldon85@gmail.com>
2014-08-18 01:32:38 +0800

    Add ping controller

---
+++ elixir-phoenix/web/controllers/ping_controller.ex
@@ -0,0 +1,13 @@
+defmodule ElixirPhoenix.PingController do
+  use Phoenix.Controller
+
+  def index(conn, _params) do
+    # connect to mongo
+    {:ok, connection} = :mongo.connect('127.0.0.1', 27017, "quipper_web_development")
+    {student} = :mongo.find_one(connection, "users", {:username, "student1"})
+    username = [username: elem(student, 7)]
+
+    conn |> assign_layout(:none)
+         |> render "index", student: username
+  end
+end
